7f476c96f409724885d2ec03522f7e2ad19b43c3
wcleungaj <90839737+RickyHKUST@users.noreply.github.com>
2021-11-21 15:34:29 +0800

    [Test Case] Tester for CheckInput

--- src/test/java/CheckInputTester.java
+++ src/test/java/CheckInputTester.java
@@ -11,34 +11,76 @@ public class CheckInputTester {
     
 	String dataset;
     String ISO_code;
-    LocalDate localdate_late,localdate_early,localdate_inRange;
+    LocalDate localdate_late_1,localdate_late_2,localdate_early_1,localdate_early_2,localdate_valid_1,localdate_valid_2;
     
     @Before
     public void setup() {
         dataset = "COVID_Dataset_v1.0.csv";
         ISO_code = "AFG";
-        localdate_late = LocalDate.now();
-        localdate_early = LocalDate.parse("2018-11-10");
-        localdate_inRange = LocalDate.parse("2020-11-10");
+        localdate_late_1 = LocalDate.now();
+        localdate_early_1 = LocalDate.parse("2018-11-10");
+        localdate_early_2 = LocalDate.parse("2017-11-10");
+        localdate_valid_1 = LocalDate.parse("2020-11-10");
+        localdate_valid_2 = LocalDate.parse("2020-11-13");
     }
 
     @Test
-    public void checkDateValidation_Null() {
+    public void checkDateValidation_InvalidInput_null() {
     	CheckInput.checkValidDate(null, dataset);
     }
     
     @Test
-    public void checkDateValidation_Late() {
-    	CheckInput.checkValidDate(localdate_late, dataset);
+    public void checkDateValidation_InvalidInput_outrange() {
+    	CheckInput.checkValidDate(localdate_late_1, dataset);
+    	CheckInput.checkValidDate(localdate_late_2, dataset);
+    	CheckInput.checkValidDate(localdate_early_1, dataset);
+    	CheckInput.checkValidDate(localdate_early_2, dataset);
+    }
+    @Test
+    public void checkDateValidation_ValidInput() {
+    	CheckInput.checkValidDate(localdate_valid_1, dataset);
     }
     
     @Test
-    public void checkDateValidation_Early() {
-    	CheckInput.checkValidDate(localdate_early, dataset);
+    public void checkPeriodValidation_ValidInput_Period() {
+    	CheckInput.checkValidPeriod(localdate_valid_1,localdate_valid_2, dataset);
     }
-    
     @Test
-    public void checkDateValidation_InRange() {
-    	CheckInput.checkValidDate(localdate_inRange, dataset);
+    public void checkPeriodValidation_ValidInput_Date() {
+    	CheckInput.checkValidPeriod(localdate_valid_1,localdate_valid_1, dataset);
+    }
+    @Test
+    public void checkPeriodValidation_InvalidInput_Outrange() {
+    	CheckInput.checkValidPeriod(localdate_early_2,localdate_valid_2, dataset);
+    	CheckInput.checkValidPeriod(localdate_early_2,localdate_early_1, dataset);
+    	CheckInput.checkValidPeriod(localdate_early_1,localdate_late_1, dataset);
+    	CheckInput.checkValidPeriod(localdate_valid_1,localdate_late_2, dataset);
+    }
+    @Test
+    public void checkPeriodValidation_InvalidInput_EndBeforeStart() {
+    	CheckInput.checkValidPeriod(localdate_valid_2,localdate_valid_1, dataset);
+    	CheckInput.checkValidPeriod(localdate_late_1,localdate_early_1, dataset);
+    }
+    @Test
+    public void checkPeriodValidation_InvalidInput_Null() {
+    	CheckInput.checkValidPeriod(null,localdate_valid_1, dataset);
+    	CheckInput.checkValidPeriod(localdate_valid_1,null, dataset);
+    	CheckInput.checkValidPeriod(null,null, dataset);
+    }
+    @Test
+    public void checkLocationValidation_ValidInput() {
+    	CheckInput.checkValidLocations(ISO_code,dataset);
+    }
+    @Test
+    public void checkLocationValidation_InvalidInput_noDataset() {
+    	CheckInput.checkValidLocations(ISO_code,null);
+    }
+    @Test
+    public void checkLocationValidation_InvalidInput_noISOcode() {
+    	CheckInput.checkValidLocations(null,dataset);
+    }
+    @Test
+    public void checkLocationValidation_InvalidInput_wrongISOcode() {
+    	CheckInput.checkValidLocations("Wakanda",dataset);
     }
 }
